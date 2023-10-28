3858ed1eeb777c2658e79791639f3682e421474f
Staymix <staszubov199530@gmail.com>
2023-10-28 11:35:06 +0600

    Add dish controller test, refactoring contr test

--- src/test/java/ru/staymix/restaurantvoting/service/DishServiceTest.java
+++ src/test/java/ru/staymix/restaurantvoting/service/DishServiceTest.java
@@ -10,8 +10,8 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static ru.staymix.restaurantvoting.testdata.DishTestData.*;
-import static ru.staymix.restaurantvoting.testdata.RestaurantTestData.REST2_ID;
-import static ru.staymix.restaurantvoting.testdata.RestaurantTestData.REST_ID;
+import static ru.staymix.restaurantvoting.testdata.RestaurantTestData.RESTAURANT2_ID;
+import static ru.staymix.restaurantvoting.testdata.RestaurantTestData.RESTAURANT_ID;
 
 class DishServiceTest extends AbstractServiceTest {
 
@@ -20,51 +20,51 @@ class DishServiceTest extends AbstractServiceTest {
 
     @Test
     void create() {
-        Dish created = service.create(getNew(), REST_ID);
+        Dish created = service.create(getNew(), RESTAURANT_ID);
         int id = created.id();
         Dish newDish = getNew();
         newDish.setId(id);
         DISH_MATCHER.assertMatch(created, newDish);
-        DISH_MATCHER.assertMatch(service.get(id, REST_ID), newDish);
+        DISH_MATCHER.assertMatch(service.get(id, RESTAURANT_ID), newDish);
     }
 
     @Test
     void delete() {
-        service.delete(DISH_ID, REST_ID);
-        assertThrows(NotFoundException.class, () -> service.get(DISH_ID, REST_ID));
+        service.delete(DISH_ID, RESTAURANT_ID);
+        assertThrows(NotFoundException.class, () -> service.get(DISH_ID, RESTAURANT_ID));
     }
 
     @Test
     void deleteNotFound() {
-        assertThrows(NotFoundException.class, () -> service.delete(DISH_NOT_FOUND, REST_ID));
+        assertThrows(NotFoundException.class, () -> service.delete(DISH_NOT_FOUND, RESTAURANT_ID));
     }
 
     @Test
     void deleteNotOwn() {
-        assertThrows(NotFoundException.class, () -> service.delete(DISH_ID, REST2_ID));
+        assertThrows(NotFoundException.class, () -> service.delete(DISH_ID, RESTAURANT2_ID));
     }
 
     @Test
     void get() {
-        Dish dish = service.get(DISH_ID, REST_ID);
+        Dish dish = service.get(DISH_ID, RESTAURANT_ID);
         DISH_MATCHER.assertMatch(dish, dish1);
     }
 
     @Test
     void getNotFound() {
-        assertThrows(NotFoundException.class, () -> service.get(DISH_NOT_FOUND, REST_ID));
+        assertThrows(NotFoundException.class, () -> service.get(DISH_NOT_FOUND, RESTAURANT_ID));
     }
 
     @Test
     void getMenuFromDate() {
-        List<Dish> menu = service.getMenuFromDate(REST_ID, LocalDate.now().minusDays(1));
+        List<Dish> menu = service.getMenuFromDate(RESTAURANT_ID, LocalDate.now().minusDays(1));
         DISH_MATCHER.assertMatch(menu, menu1);
     }
 
     @Test
     void update() {
         Dish updated = getUpdated();
-        service.update(updated, REST_ID);
-        DISH_MATCHER.assertMatch(service.get(DISH_ID, REST_ID), updated);
+        service.update(updated, RESTAURANT_ID);
+        DISH_MATCHER.assertMatch(service.get(DISH_ID, RESTAURANT_ID), updated);
     }
 }
---
+++ src/test/java/ru/staymix/restaurantvoting/web/dish/AdminDishControllerTest.java
@@ -0,0 +1,187 @@
+package ru.staymix.restaurantvoting.web.dish;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import ru.staymix.restaurantvoting.error.NotFoundException;
+import ru.staymix.restaurantvoting.model.Dish;
+import ru.staymix.restaurantvoting.service.DishService;
+import ru.staymix.restaurantvoting.util.JsonUtil;
+import ru.staymix.restaurantvoting.web.AbstractControllerTest;
+import ru.staymix.restaurantvoting.web.restaurant.AdminRestaurantController;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static ru.staymix.restaurantvoting.testdata.DishTestData.*;
+import static ru.staymix.restaurantvoting.testdata.RestaurantTestData.RESTAURANT_ID;
+import static ru.staymix.restaurantvoting.testdata.UserTestData.ADMIN_MAIL;
+import static ru.staymix.restaurantvoting.testdata.UserTestData.USER_MAIL;
+
+class AdminDishControllerTest extends AbstractControllerTest {
+
+    private static final String REST_URL_SLASH = AdminRestaurantController.REST_URL + '/';
+    private static final String REST_DISH_URL_SLASH = "/dishes/";
+
+    @Autowired
+    private DishService service;
+
+    @Test
+    @WithUserDetails(ADMIN_MAIL)
+    void createWithLocation() throws Exception {
+        Dish newDish = getNew();
+        ResultActions action = perform(MockMvcRequestBuilders.post(REST_URL_SLASH + RESTAURANT_ID + "/dishes")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(JsonUtil.writeValue(newDish)))
+                .andExpect(status().isCreated());
+
+        Dish created = DISH_MATCHER.readFromJson(action);
+        int id = created.id();
+        newDish.setId(id);
+        DISH_MATCHER.assertMatch(created, newDish);
+        DISH_MATCHER.assertMatch(service.get(id, RESTAURANT_ID), newDish);
+    }
+
+    @Test
+    @WithUserDetails(ADMIN_MAIL)
+    void createInvalid() throws Exception {
+        Dish invalid = getNew();
+        invalid.setName(null);
+        perform(MockMvcRequestBuilders.post(REST_URL_SLASH + RESTAURANT_ID + "/dishes")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(JsonUtil.writeValue(invalid)))
+                .andExpect(status().isUnprocessableEntity());
+    }
+
+    @Test
+    @WithUserDetails(ADMIN_MAIL)
+    void createDuplicate() throws Exception {
+        Dish invalid = getNew();
+        invalid.setName(dish2.getName());
+        perform(MockMvcRequestBuilders.post(REST_URL_SLASH + RESTAURANT_ID + "/dishes")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(JsonUtil.writeValue(invalid)))
+                .andExpect(status().isConflict());
+    }
+
+    @Test
+    @WithUserDetails(ADMIN_MAIL)
+    void delete() throws Exception {
+        perform(MockMvcRequestBuilders.delete(REST_URL_SLASH + RESTAURANT_ID + REST_DISH_URL_SLASH + DISH_ID))
+                .andDo(print())
+                .andExpect(status().isNoContent());
+        assertThrows(NotFoundException.class, () -> service.get(DISH_ID, RESTAURANT_ID));
+    }
+
+    @Test
+    @WithUserDetails(ADMIN_MAIL)
+    void deleteNotFound() throws Exception {
+        perform(MockMvcRequestBuilders.delete(REST_URL_SLASH + DISH_NOT_FOUND))
+                .andDo(print())
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    @WithUserDetails(USER_MAIL)
+    void deleteForbidden() throws Exception {
+        perform(MockMvcRequestBuilders.delete(REST_URL_SLASH + DISH_NOT_FOUND))
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    @WithUserDetails(ADMIN_MAIL)
+    void get() throws Exception {
+        perform(MockMvcRequestBuilders.get(REST_URL_SLASH + RESTAURANT_ID + REST_DISH_URL_SLASH + DISH_ID))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(DISH_MATCHER.contentJson(dish1));
+    }
+
+    @Test
+    @WithUserDetails(ADMIN_MAIL)
+    void getNotFound() throws Exception {
+        perform(MockMvcRequestBuilders.get(REST_URL_SLASH + RESTAURANT_ID + REST_DISH_URL_SLASH + DISH_NOT_FOUND))
+                .andDo(print())
+                .andExpect(status().isNotFound());
+
+    }
+
+    @Test
+    void getUnAuth() throws Exception {
+        perform(MockMvcRequestBuilders.get(REST_URL_SLASH + RESTAURANT_ID + REST_DISH_URL_SLASH))
+                .andDo(print())
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    @WithUserDetails(ADMIN_MAIL)
+    void getMenuFromDate() throws Exception {
+        perform(MockMvcRequestBuilders.get(REST_URL_SLASH + RESTAURANT_ID + "/dishes")
+                .param("date", LocalDate.now().minusDays(1).toString()))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(DISH_MATCHER.contentJson(menu1));
+
+    }
+
+    @Test
+    @WithUserDetails(ADMIN_MAIL)
+    void update() throws Exception {
+        Dish updated = getUpdated();
+        perform(MockMvcRequestBuilders.put(REST_URL_SLASH + RESTAURANT_ID + REST_DISH_URL_SLASH + DISH_ID)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(JsonUtil.writeValue(updated)))
+                .andDo(print())
+                .andExpect(status().isNoContent());
+
+        DISH_MATCHER.assertMatch(service.get(DISH_ID, RESTAURANT_ID), updated);
+    }
+
+    @Test
+    @WithUserDetails(ADMIN_MAIL)
+    void updateInvalid() throws Exception {
+        Dish invalid = getUpdated();
+        invalid.setName("");
+        perform(MockMvcRequestBuilders.put(REST_URL_SLASH + RESTAURANT_ID + REST_DISH_URL_SLASH + DISH_ID)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(JsonUtil.writeValue(invalid)))
+                .andDo(print())
+                .andExpect(status().isUnprocessableEntity());
+    }
+
+    @Test
+    @WithUserDetails(ADMIN_MAIL)
+    void updateHtmlUnsafe() throws Exception {
+        Dish invalid = getUpdated();
+        invalid.setName("<script>alert(123)</script>");
+        perform(MockMvcRequestBuilders.put(REST_URL_SLASH + RESTAURANT_ID + REST_DISH_URL_SLASH + DISH_ID)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(JsonUtil.writeValue(invalid)))
+                .andDo(print())
+                .andExpect(status().isUnprocessableEntity());
+    }
+
+    @Test
+    @WithUserDetails(ADMIN_MAIL)
+    void updateDuplicate() throws Exception {
+        Dish invalid = getUpdated();
+        invalid.setName(dish2.getName());
+        perform(MockMvcRequestBuilders.put(REST_URL_SLASH + RESTAURANT_ID + REST_DISH_URL_SLASH + DISH_ID)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(JsonUtil.writeValue(invalid)))
+                .andDo(print())
+                .andExpect(status().isNoContent());
+
+        perform(MockMvcRequestBuilders.get(REST_URL_SLASH + RESTAURANT_ID + "/dishes")
+                .param("date", LocalDate.now().minusDays(1).toString()))
+                .andExpect(status().isConflict())
+                .andDo(print());
+    }
+}
